ff117eb4201e3ee1714131011a93802eadee5af1
Samarth01 <samarthsolanki56@gmail.com>
2025-07-03 17:46:51 +0530

    [Refactor Step 1] Single Number to return that number and two Number should return sum excluding WhiteSpaces

--- src/main/java/com/StringCalculator/StringCalculator.java
+++ src/main/java/com/StringCalculator/StringCalculator.java
@@ -4,6 +4,11 @@ public class StringCalculator {
 
     public int add(String numbers){
         if(numbers.isEmpty()) return 0;
-        return -1;
+       String[] parts = numbers.split(",");
+       int sum = 0;
+       for (String part:parts){
+           sum += Integer.parseInt(part.trim());
+       }
+       return sum;
     }
 }
--- src/test/java/com/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/StringCalculator/StringCalculatorTest.java
@@ -9,5 +9,15 @@ public class StringCalculatorTest {
     void test_Empty_String_Should_Return_Zero(){
         assertEquals(0,calc.add(""));
     }
+    @Test
+    void test_Single_Number_Should_Return_That_Number(){
+        assertEquals(1,calc.add(" 1"));
+        assertEquals(82,calc.add("82    "));
+    }
+    @Test
+    void test_Two_Numbers_Should_Return_Sum(){
+        assertEquals(6,calc.add("3,3"));
+        assertEquals(30,calc.add("  10,20  "));
+    }
 
 }
