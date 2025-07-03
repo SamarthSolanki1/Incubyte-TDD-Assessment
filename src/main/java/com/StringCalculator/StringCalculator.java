81c98a5a4f5fa578e34b217d51f75f27afdef617
Samarth01 <samarthsolanki56@gmail.com>
2025-07-03 22:28:44 +0530

    [passed case step-4] Different delimeter should work

--- src/main/java/com/StringCalculator/StringCalculator.java
+++ src/main/java/com/StringCalculator/StringCalculator.java
@@ -1,14 +1,25 @@
 package com.StringCalculator;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
     public int add(String numbers){
-        if(numbers.isEmpty()) return 0;
-       String[] parts = numbers.split("[,\n]");
-       int sum = 0;
-       for (String part:parts){
-           sum += Integer.parseInt(part.trim());
-       }
+        if(numbers == null || numbers.isEmpty()) return 0;
+
+       String delimiter = "[,\n]"; // this is default delimiter
+        if(numbers.startsWith("//")){
+            int delimiterEndIndex = numbers.indexOf("\n");
+            String customdelimiter = numbers.substring(2,delimiterEndIndex);
+            delimiter = Pattern.quote(customdelimiter);
+            numbers = numbers.substring(delimiterEndIndex + 1);
+        }
+        String[] parts = numbers.split(delimiter);
+        int sum = 0;
+
+        for (String part : parts) {
+            sum += Integer.parseInt(part.trim());
+        }
+
        return sum;
     }
 }
